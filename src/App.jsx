eae9e23511c9d685493e973f938692321303c1d1
JeremyBaumiller <jbaumil07@gmail.com>
2024-04-09 22:22:46 -0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ContactList from './components/ContactList'; // Importing the ContactList component
+
+export default function App() {
+  return (
+    <div className="App">
+      {}
+      <ContactList /> {/* Using the ContactList component */}
+    </div>
+  );
+}
+
+
